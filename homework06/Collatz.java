662d67ae4c79c4cb336ad1c0e57c7a9d0797bbb9
cristianornelas2 <46763991+cristianornelas2@users.noreply.github.com>
2019-04-15 18:41:25 -0700

    Update Collatz.java

--- homework06/Collatz.java
+++ homework06/Collatz.java
@@ -1,38 +1,121 @@
 import java.io.*; 
-  
-class Collatz  { 
- 
- BrobInt CollatzNumber; 
- public Collatz(BrobInt n){
-    CollatzNumber = n;
- }
-    public void printCollatz() {
-        BrobInt one = new BrobInt("1"); 
-        BrobInt two = new BrobInt("2"); 
-        BrobInt three = new BrobInt("3");  
-        BrobInt counter = new BrobInt("1");
-        System.out.println("Before While Loop ");
-        while (CollatzNumber.compareTo(one) == 1) { 
-            System.out.print("\n step = ");
-            System.out.println(counter.toString());
-            System.out.println(CollatzNumber.toString()); 
-            System.out.println("1");
-            if ((CollatzNumber.remainder(two)).equals(one)) {
-                System.out.println("2");
-                CollatzNumber = CollatzNumber.multiply(three);
-                System.out.println("3"); 
-                CollatzNumber = CollatzNumber.addInt(one);
-                System.out.println("4");
+
+public class Collatz {
+     private BrobInt CollatzNumber = null; 
+        
+        private static String one = "1";
+        private static String two = "2";
+        private static String three = "3";
+
+        public Collatz(String value){
+            CollatzNumber = new BrobInt(value);
+        }
+
+        public BrobInt longDivision(BrobInt gint, int b)
+        {
+            int[] c = new int [10000];
+            String dividend = gint.toString();
+            
+            int la;
+            int i,k=0,flag=0;
+            int temp=1;
+            la=dividend.length();
+            
+            int [] a = new int[la];
+
+            for(i=0;i<la;i++){
+                a[i] = dividend.charAt(i) - 48;
             }
-            else {
-                System.out.println("5");
-                CollatzNumber = CollatzNumber.divide(two); 
-                System.out.println("6");
-            }
-            System.out.println("7");
-            counter = counter.addInt(one);
-            System.out.println("8");
-            break;
+
+            temp = a[0];
+    
+           
+            for(i=1;i<=la;i++){
+
+             if(b<=temp){
+                 c[k++] = temp/b;
+                 temp = temp % b;
+
+                 if(i<la)
+                     temp =temp*10 + a[i];
+                 else
+                     temp =temp*10;
+                 flag=1;
+
+             }
+             else{
+                 if(i<la)
+                     temp =temp*10 + a[i];
+                 else
+                     temp =temp*10;
+                 
+                 if(flag==1)
+                     c[k++] = 0;
+             }
+        }
+
+        String strArray = "";
+        
+        for(int j=0;j<k;j++){
+            strArray += String.valueOf(c[j]);
+        }
+
+        String ans = strArray;
+
+        BrobInt d = new BrobInt(ans);
+
+        return d;
+
+        }
+
+
+        public void printCollatz() {
+            
+            BrobInt one_ = new BrobInt(one); 
+            BrobInt two_ = new BrobInt(two); 
+            BrobInt three_ = new BrobInt(three);
+
+            BrobInt counter = new BrobInt(one);
+            
+            // We simply follow steps 
+            // while we do not reach 1 
+
+            while (CollatzNumber.compareTo(one_) > 0 ){ 
+
+                System.out.print("\n step = ");
+                System.out.println(counter.toString());
+                System.out.println(CollatzNumber.toString()); 
+                
+                
+                
+
+                String r = CollatzNumber.toString();
+                int lastIndx = r.length();
+                
+                if(lastIndx == 0) break;
+
+                //System.out.println(lastIndx);
+                char msb = r.charAt(lastIndx-1);
+
+                // If n is odd 
+                if (msb == '1' || msb == '3' || msb == '5' || msb == '7' || msb == '9') {
+                    
+                    CollatzNumber = CollatzNumber.multiply(three_);
+                    
+                    CollatzNumber = CollatzNumber.add(one_);
+                    
+                }
+          
+                // If even 
+                else {
+                    
+                    CollatzNumber = longDivision(CollatzNumber, 2); 
+                    
+                }
+                // Print 1 at the end
+                
+                counter = counter.add(one_);
+                
+            } 
         } 
-    } 
-} 
+}
